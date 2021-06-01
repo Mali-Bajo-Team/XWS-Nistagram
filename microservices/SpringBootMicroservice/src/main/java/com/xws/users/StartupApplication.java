d02163f647799e65adcb9621f44b92b1d096baba
Pufke <pualic.nemanja@gmail.com>
2021-06-01 02:37:35 +0200

    feat: restart spring boot microservice untill db is ready

--- microservices/SpringBootMicroservice/src/main/java/com/xws/users/StartupApplication.java
+++ microservices/SpringBootMicroservice/src/main/java/com/xws/users/StartupApplication.java
@@ -1,7 +1,15 @@
 package com.xws.users;
 
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class StartupApplication {
@@ -9,5 +17,26 @@ public class StartupApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StartupApplication.class, args);
 	}
+
+	@Bean
+	public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+		return bf -> {
+			// Let beans that need the database depend on the DatabaseStartupValidator
+			// like the JPA EntityManagerFactory or Flyway
+			String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
+			Stream.of(jpa)
+					.map(bf::getBeanDefinition)
+					.forEach(it -> it.setDependsOn("databaseStartupValidator"));
+		};
+	}
+
+	@Bean
+	public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
+		var dsv = new DatabaseStartupValidator();
+		dsv.setDataSource(dataSource);
+		dsv.setValidationQuery(DatabaseDriver.POSTGRESQL.getValidationQuery());
+		return dsv;
+	}
+
 
 }
