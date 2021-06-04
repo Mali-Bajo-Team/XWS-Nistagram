7475215327ca1ef66e9fbd0e18f1f946783988eb
vlaksi <daksrapsody@gmail.com>
2021-06-04 17:49:37 +0200

    feature[content-service]: added few attributes and entities in model

--- microservices/ContentService/model/user.go
+++ microservices/ContentService/model/user.go
@@ -3,18 +3,18 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID           primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
-	Username     string             `json:"username,omitempty" bson:"username,omitempty"`
-	ProfileImage string             `json:"profile_image,omitempty" bson:"profile_image,omitempty"`
-	IsPrivate    bool               `json:"is_private,omitempty" bson:"is_private,omitempty"`
+	ID             primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
+	Username       string             `json:"username,omitempty" bson:"username,omitempty"`
+	ProfileImage   string             `json:"profile_image,omitempty" bson:"profile_image,omitempty"`
+	IsPrivate      bool               `json:"is_private,omitempty" bson:"is_private,omitempty"`
 
-	//
-	///** @pdRoleInfo migr=no name=Collection assc=association12 mult=0..* */
-	//public Collection[] collection;
-	///** @pdRoleInfo migr=no name=Post assc=association7 mult=0..* */
-	//public Post[] posts;
-	///** @pdRoleInfo migr=no name=RegularPost assc=association10 mult=0..* */
-	//public RegularPost[] favorites;
-	///** @pdRoleInfo migr=no name=StoryHighlight assc=association13 mult=0..* */
+	Posts          []Post             `json:"posts,omitempty" bson:"posts,omitempty"`
+	PostsRef       []interface{}      `json:"posts_ref,omitempty" bson:"posts_ref,omitempty"`
+	Collections    []Collection       `json:"collections,omitempty" bson:"collections,omitempty"`
+	CollectionsRef []interface{}      `json:"collections_ref,omitempty" bson:"collections_ref,omitempty"`
+	Favorites      []RegularPost      `json:"favorites,omitempty" bson:"favorites,omitempty"`
+	FavoritesRef   []interface{}      `json:"favorites_ref,omitempty" bson:"favorites_ref,omitempty"`
+
+
 	//public StoryHighlight[] storyHighlights;
 }
