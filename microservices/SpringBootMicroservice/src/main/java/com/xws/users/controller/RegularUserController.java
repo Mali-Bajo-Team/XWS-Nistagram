01f45f2e416b993358f8d57c01f5f7031996dee3
Pufke <pualic.nemanja@gmail.com>
2021-06-06 21:10:23 +0200

    chore[users-services] initial backand api for update Regular user

--- microservices/SpringBootMicroservice/src/main/java/com/xws/users/controller/RegularUserController.java
+++ microservices/SpringBootMicroservice/src/main/java/com/xws/users/controller/RegularUserController.java
@@ -1,18 +1,40 @@
 package com.xws.users.controller;
 
+import com.xws.users.dto.RegularUserUpdateDTO;
+import com.xws.users.service.impl.RegularUserService;
+import com.xws.users.users.model.roles.RegularUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/regularuser")
 public class RegularUserController {
 
+    @Autowired
+    private RegularUserService regularUserService;
+
+
     @PreAuthorize("hasRole('REGULAR')")
     @GetMapping("/test")
     public ResponseEntity<String> getUsersAccs() {
         return ResponseEntity.ok("Test test!");
+    }
+
+    @PreAuthorize("hasRole('REGULAR')")
+    @PutMapping(consumes = "application/json")
+    public ResponseEntity<RegularUserUpdateDTO> updateUserAcc(@RequestBody RegularUser regularUser) {
+
+        RegularUser regularUserForUpdate = regularUserService.findByUsername(regularUser.getUsername());
+
+        if (regularUserForUpdate == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        regularUserForUpdate.setName(regularUser.getName());
+
+        regularUser = regularUserService.save(regularUserForUpdate);
+        return new ResponseEntity<>(new RegularUserUpdateDTO(regularUser), HttpStatus.OK);
     }
 }
--- microservices/SpringBootMicroservice/src/main/java/com/xws/users/service/impl/RegularUserService.java
+++ microservices/SpringBootMicroservice/src/main/java/com/xws/users/service/impl/RegularUserService.java
@@ -3,6 +3,7 @@ package com.xws.users.service.impl;
 import com.xws.users.repository.IRegularUserRepository;
 import com.xws.users.service.IRegularUserService;
 import com.xws.users.users.model.roles.RegularUser;
+import com.xws.users.users.model.roles.UserAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +16,6 @@ public class RegularUserService implements IRegularUserService {
     public RegularUser findByUsername(String username) {
         return regularUserRepository.findByUsername(username);
     }
+
+    public RegularUser save(RegularUser regularUser) { return regularUserRepository.save(regularUser); }
 }
