2945a98c347ebbb4a7be158e9aa948b54818f0aa
Milana Todorovic <milanatodorovic@uns.ac.rs>
2021-06-18 20:29:41 +0200

    refactor: added post component

--- frontend-vue-js/src/components/Navbar.vue
+++ frontend-vue-js/src/components/Navbar.vue
@@ -1,31 +1,24 @@
 <template>
-    <v-app-bar
-      fixed    
-      scroll-threshold="80" 
-      app      
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top, rgba(255,255,230,.9), rgba(15,80,80,.1)"          
-            src="@/assets/social.jpg"
-        ></v-img>
-      </template>
+  <v-app-bar fixed scroll-threshold="80" app>
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top, rgba(255,255,230,.9), rgba(15,80,80,.1)"
+        src="@/assets/social.jpg"
+      ></v-img>
+    </template>
 
-      <template v-slot:extension>
-        <v-tabs align-with-title hide-slider>
-          <template v-for="item in items">
-            <v-tab v-if="item.to" :key="item.title" :to="item.to">{{item.title}}</v-tab>
-            <v-menu  v-else :key="item.title" bottom offset-y left>
+    <template v-slot:extension>
+      <v-tabs align-with-title hide-slider>
+        <template v-for="item in items">
+          <v-tab v-if="item.to" :key="item.title" :to="item.to">{{
+            item.title
+          }}</v-tab>
+          <v-menu v-else :key="item.title" bottom offset-y left>
             <template v-slot:activator="{ on, attrs }">
-              <v-tab
-                v-bind="attrs"
-                v-on="on"                
-              >
-                {{item.title}}
-                <v-icon right>
-                  mdi-menu-down
-                </v-icon>
+              <v-tab v-bind="attrs" v-on="on">
+                {{ item.title }}
+                <v-icon right> mdi-menu-down </v-icon>
               </v-tab>
             </template>
 
@@ -40,32 +33,34 @@
               </v-list-item>
             </v-list>
           </v-menu>
-          </template>        
-          <v-tab v-if="logout" @click="doLogout">Log out</v-tab>
-        </v-tabs>
-      </template>
-    </v-app-bar>
+        </template>
+        <v-tab v-if="logout" @click="doLogout">Log out</v-tab>
+      </v-tabs>
+    </template>
+  </v-app-bar>
 </template>
 
 <script>
+import { removeToken } from "../util/token";
 
 export default {
-    props: {
-        items: {
-            type: Array,
-            default: function () {
-                return []
-            }
-        },
-        logout: {
-          type: Boolean,
-          default: true
-        }
+  props: {
+    items: {
+      type: Array,
+      default: function () {
+        return [];
+      },
     },
-    methods:{
-      doLogout: function() {
-        this.$router.push('/')
-      }
-    }
-}
+    logout: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  methods: {
+    doLogout: function () {
+      removeToken();
+      this.$router.push("/");
+    },
+  },
+};
 </script>
