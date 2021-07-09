353c382e97d323f03184c668bae87bb958d8178f
anciz <anaperisictb@yahoo.com>
2021-07-09 18:38:47 +0200

    feature: finished front part for agent request approve

--- frontend-vue-js/src/pages/admin/agentRequests/AgentRegistrationRequests.vue
+++ frontend-vue-js/src/pages/admin/agentRequests/AgentRegistrationRequests.vue
@@ -5,15 +5,19 @@
 		<v-col sm="8" md="12" lg="4" cols="12">
 			<v-row>
 				<v-col>
-					<v-card>
+					<v-card
+						v-for="request in requests"
+						:key="request.username"
+						style="margin:1em;"
+					>
 						<!--Information:usernames and date about content-->
 						<v-card-text>
 							Valid email:
-							<b>ivkica@gmail.com</b>
+							<b>{{ request.email }}</b>
 							<br />
-							The person who created this request:<b> username</b>
+							The person who created this request:<b> {{ request.username }}</b>
 							<br />
-							Link to the web site: <b> https://stagod/samo/da/radi</b>
+							Link to the web site: <b> {{ request.linkToWebSite }}</b>
 						</v-card-text>
 
 						<!--End of information about  content-->
@@ -23,7 +27,13 @@
 						<!--Buttons-->
 						<v-card-actions>
 							<!--Button for removing content-->
-							<v-btn class="ma-2" outlined rounded color="primary">
+							<v-btn
+								class="ma-2"
+								outlined
+								rounded
+								color="primary"
+								@click="approveRequest(request)"
+							>
 								<v-icon>
 									mdi-check-circle
 								</v-icon>
@@ -38,3 +48,53 @@
 		<v-spacer></v-spacer>
 	</v-row>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			requests: [],
+		};
+	},
+	mounted() {
+		this.axios
+			.get(
+				process.env.VUE_APP_BACKEND_URL + process.env.VUE_APP_AGENT_REQUEST,
+				{
+					headers: {
+						Authorization: 'Bearer ' + localStorage.getItem('JWT-CPIS'),
+					},
+				}
+			)
+			.then((response) => {
+				this.requests = response.data;
+				console.log(this.requests);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	},
+	methods: {
+		approveRequest(request) {
+			this.axios
+				.post(
+					process.env.VUE_APP_BACKEND_URL +
+						process.env.VUE_APP_AGGENT_ACCEPT_REGISTRATION,
+					{
+						username: request.username,
+					}
+				)
+				.then((res) => {
+					console.log(res);
+					this.requests.filter(
+						(tempRequest) => tempRequest.username !== request.username
+					);
+					location.reload();
+				})
+				.catch((e) => {
+					console.log(e);
+				});
+		},
+	},
+};
+</script>
